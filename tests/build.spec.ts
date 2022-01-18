3558ef58a7d2f7d6862a46e95f1679ed70d4ec7f
Maximo Mussini <maximomussini@gmail.com>
2022-01-18 16:21:50 -0300

    test: use vitest to check build result

---
+++ tests/build.spec.ts
@@ -0,0 +1,40 @@
+import { readFileSync } from 'fs'
+import { resolve } from 'path'
+import spawn from 'cross-spawn'
+import { beforeAll, describe, test, expect } from 'vitest'
+
+const exampleRoot = resolve(__dirname, '../example')
+
+describe('build', () => {
+  beforeAll(() => buildApp(), 5000)
+
+  test('adds integrity hash', () => {
+    const manifest = readManifest('manifest.json')
+    expect(manifest['entrypoints/main.ts'].integrity)
+      .toEqual('sha384-2A5vUNf7cFDCWm6RTDPAnr/wmGjkQhXz4EP5keVPYX4OnI2Ws1iXgTQ70CTmC1Ux')
+
+    expect(manifest['dynamic.ts'].integrity)
+      .toEqual('sha384-KorBstcdnwE8bqEM96n5weR+Q9mNUAR0Hr7tewyfRadn5v1IY58p2bfjmnkxTotE')
+
+    const assetsManifest = readManifest('manifest-assets.json')
+    expect(assetsManifest['entrypoints/styles.css'].integrity)
+      .toEqual('sha384-HmlJ0WJXVNLaqWQYVRhZgsW1JBN4XBjm3j+j38nagNFimvBxi8nZ1FUysAc9JJvL')
+  })
+
+  test('manifest snapshot', () => {
+    expect(readAsset('manifest.json')).toMatchSnapshot()
+    expect(readAsset('manifest-assets.json')).toMatchSnapshot()
+  })
+})
+
+function readManifest (path: string) {
+  return JSON.parse(readAsset(path))
+}
+
+function readAsset (path: string) {
+  return readFileSync(`${exampleRoot}/public/vite/${path}`, 'utf-8')
+}
+
+function buildApp () {
+  spawn.sync('npm', ['run', 'build'], { stdio: process.env.DEBUG ? 'inherit' : undefined, cwd: exampleRoot })
+}
